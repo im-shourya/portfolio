da55be97c5ca064ca7041957647a8f99a4138dd9
Shourya <parasharshourya07@gmail.com>
2026-05-05 23:57:21 +0530

    fix the render isuuse of paint stroke

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,6 +5,7 @@ import Services from '@/components/Services';
 export default function ServicesPage() {
   return (
     <main className="min-h-screen flex flex-col">
+      
       <Header />
       <div className="flex-1">
         <Services />
